bbf79b45929ae2d4b404115b0b25978a2aa1d1d2
B1Git <ericrittasouza@gmail.com>
2025-06-16 18:08:46 -0300

    sistema do jogo em si em progesso

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ import {Keyboards, KeyboardNames} from "./scripts/keyboards.js";
 
 // Globals
 global.print = console.log;
-global.write = process.stdout.write;
+global.clear = console.clear;
 
 // Game constants
 const Keyboard = new Keyboards;
@@ -22,7 +22,13 @@ let Rules;
 let Cards;
 let Money;
 let Round;
-let Blind;
+let Typos;
+let HighestEnter;
+
+// Functions 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
 
 // Game functions
 function selectKeyboard() {
@@ -38,26 +44,42 @@ function selectKeyboard() {
   print(`Keyboard ${color ? chalk[color].bold(Keyboard.Selected) : Keyboard.Selected} was selected!`);
 };
 
-function resetGame() {
-  Rules = new gameRules;
+async function startGame() {
   Cards = new InGameCards;
   Money = 0;
   Round = 0;
-  Blind = 300;
-};
+  Typos = 0;
+  HighestEnter = 0;
+  print(`\n${chalk.green.italic.bold("Let the game begin!")}`);
+  await sleep(1500);
 
-function startGame() {
-  Rules = new gameRules;
-  Cards = new InGameCards;
-  Money = 0;
-  Round = 0;
-  Blind = 300;
-
-  
-
+  while (true) {
+    newRound()
+    if (Rules.gameTotalPoints >= Typos) {continue};
+    print(`\n${chalk.red.italic.bold("Game over!")}`);
+    print('Your highest played enter was: ');
+    print(`${chalk.green.bold(HighestEnter)} Total Points!`);
+    break;
+  };
 };
 
 function newRound() {
+  if (!Cards) {throw new Error("Jogo começou sem as Cartas.")};
+  Rules = new gameRules;
+  Keyboard.changeRules(Rules);
+  Rules.getKeys();
+
+  let playing = true
+  while (playing) {
+    if (Rules.gameEnters < 1 || Rules.gameTotalPoints >= Typos) {playing = false; break};
+    clear();
+    print(``)
+    print(`${Rules.gamePoints} x ${Rules.gameMultiplier}`);
+    print(`Enters: ${Rules.gameEnters}`);
+    print(`Deletes: ${Rules.gameDeletes}`);
+    print(`Letters: ${Rules.gameKeys}`);
+    const response = prompt(`:`);
+  };
 };
 
 // Entrance message
@@ -79,8 +101,8 @@ $$ $$\\$$ |$$$  / \\$$$ |$$ |      $$ |  $$ |  $$ |       $$ |
 print(`\n${chalk.blue.italic.bold('Welcome to QWERTY!')}`);
 
 // Game loop
-let gaming = true
-while (gaming) {
+let running = true
+while (running) {
   print(
   `\nWhat do you want to do:
   (1) Start the game.
@@ -91,7 +113,7 @@ while (gaming) {
 
   switch (response) {
     case 1:
-      print("Gaming")
+      startGame();
       break;
     case 2:
       selectKeyboard();
@@ -102,8 +124,8 @@ while (gaming) {
     default:
       print("Are you sure you want to exit the game? (y/n)");
       let exit = prompt(":");
-      if (exit === null) {gaming = false};
-      if (exit.toLowerCase() === "y") {gaming = false};
+      if (exit === null) {running = false};
+      if (exit.toLowerCase() === "y") {running = false};
       break;
   };
 };
