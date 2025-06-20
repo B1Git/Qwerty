4fc4e0ede0c831c3c6c35f5cf904e7ce46d538a5
B1Git <ericrittasouza@gmail.com>
2025-06-19 21:15:37 -0300

    melhorei o desing dos menus

--- main.js
+++ main.js
@@ -75,13 +75,18 @@ async function newRound() {
 
     // board do jogo
     clear();
-    print(`${chalk.bold.inverse(`Typos: ${MinRequiredPoints}`)}`);
+    print(`${chalk.underline.bold("Typos:")} ${MinRequiredPoints}`);
     print(`\n${chalk.magenta.bold(Rules.gameScore)}`);
     print(`${chalk.blue.bold(Rules.gamePoints)} x ${chalk.red.bold(Rules.gameMultiplier)}`);
     print(`\n${chalk.green.bold('ENTER:')} ${Rules.gameEnters}`);
     print(`${chalk.red.bold("DELETE:")} ${Rules.gameDeletes}`);
-    print(`[LETTERS: ${Rules.gameKeys}]`)
-    print(`\n(1) to see your Keyboard; (2) to see your Cards;`)
+    print(`${chalk.yellow.bold('LETTERS')}: [${Rules.gameKeys}]`)
+
+    print(`\nYou can write a word, or type:`);
+    print(`(1) To ${chalk.red.bold('DELETE')} specified Letters.`);
+    print(`(2) To ${chalk.cyan.bold('ALT + TAB')} your Keys.`);
+    print(`(3) To ${chalk.yellow.bold('CTRL + F')} your Keyboards.`);
+    print(`(0) To ${chalk.magenta.bold('ESC')} the Game.`);
     const word = prompt(`:`);
     
     // Opções de escolha
@@ -92,11 +97,18 @@ async function newRound() {
     };
     
     // Confirmação da palavra
-    print(`\nAre you sure you want to ${chalk.italic.bold('ENTER')} ${word}?`);
+    const upperWord = word.toUpperCase();
+    let wordArray = [...upperWord];
+    let includeKeys = this.gameKeys.filter((item) => {
+      return wordArray.includes(item);
+    });
+
+    print(`\nAre you sure you want to ${chalk.green.italic.bold('ENTER')} ${word}?\n`);
+    if (includeKeys.length > 0) {print(`You'll ${chalk.red.italic.bold('DELETE')} the [${includeKeys}] letters.`)};
     if (!dictionary.check(word)) {print(chalk.red.bold(`${word} isn't a real word.`))};
-    print(`(N/n to ${chalk.italic.bold('not ENTER')}.)`)
+    print(`\n${chalk.green.italic.bold("ENTER")} to confirm.`)
     const response = prompt(":");
-    if (response === null || response.toLowerCase() === "n") {continue};
+    if (response !== "") {continue};
     
     // Jogada da palavra
     Rules.gameWord = [word, dictionary.check(word)];
@@ -106,20 +118,6 @@ async function newRound() {
     // Score calculation
     Rules.gameScore = Rules.gamePoints * Rules.gameMultiplier;
     clear();
-    print(chalk.magenta.bold.italic(`0 x 0`));
-    print(`${chalk.blue.bold(Rules.gamePoints)} x ${chalk.red.bold(Rules.gameMultiplier)}`);
-    await system.sleep(1000);
-    print(chalk.magenta.bold.italic(`${Rules.gamePoints} x 0`));
-    Rules.gamePoints = 0;
-    print(`${chalk.blue.bold(Rules.gamePoints)} x ${chalk.red.bold(Rules.gameMultiplier)}`);
-    await system.sleep(1000);
-    print(chalk.magenta.bold.italic(`${Rules.gamePoints} x ${Rules.gameMultiplier}`));
-    Rules.gameMultiplier = 0;
-    print(`${chalk.blue.bold(Rules.gamePoints)} x ${chalk.red.bold(Rules.gameMultiplier)}`);
-    await system.sleep(1000);
-    print(chalk.magenta.bold.italic(Rules.gameScore));
-    print(`${chalk.blue.bold(Rules.gamePoints)} x ${chalk.red.bold(Rules.gameMultiplier)}`);
-    await system.sleep(500);
   };
 };
 
@@ -166,12 +164,11 @@ while (running) {
     clear();
     print(chalk.blue.italic.bold("\\QWERTY/"));
   };
-  print(
-  `\nWhat do you want to do:
-  (1) Start the game.
-  (2) Choose keyboard.
-  (3) How to play.
-  (0) Exit game.`);
+  print(`\nWhat do you wish to do:`);
+  print(`(1) To ${chalk.green.bold("ENTER")} the Game.`);
+  print(`(2) To ${chalk.blue.bold("SELECT")} a Keyboard.`);
+  print(`(3) To ${chalk.yellow.bold("LEARN")} the Game.`);
+  print(`(0) To ${chalk.red.bold("ESC")} the Game.`);
   firstTime = false;
   let response = prompt(":");
 
@@ -189,7 +186,7 @@ while (running) {
       running = false;
       break;
     default:
-      print("Are you sure you want to exit the game? (Y/N)");
+      print(chalk.red.bold('Are you sure you want to exit the game? (Y/N)'));
       let exit = prompt(":");
       if (exit === null) {running = false};
       if (exit.toLowerCase() === "y") {running = false};
